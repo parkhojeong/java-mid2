ba91b950ca5bc53ce65d1c456b8e44a8bc25b517
hj <parkhj062@gmail.com>
2025-12-29 19:38:00 +0900

    [13. 타입 매개변수 제한3 - 제네릭 도입과 실패]
    - <T> assume Object method allowed
    - need restriction like <Integer>

---
+++ src/generic/test/ex3/AnimalHospitalV2.java
@@ -0,0 +1,31 @@
+package generic.test.ex3;
+
+import generic.animal.Animal;
+
+public class AnimalHospitalV2<T> {
+    private T animal;
+
+    public void set(T animal) {
+        this.animal = animal;
+    }
+
+    public T get() {
+        return animal;
+    }
+
+    public void checkup() {
+        // only Object method possible
+        animal.toString();
+        animal.equals(null);
+
+//        System.out.println(animal.getName());
+//        System.out.println(animal.getSize());
+//        animal.sound();
+    }
+
+    public T bigger(T target) {
+//        return animal.getSize() > target.getSize() ? animal : target;
+        return null;
+    }
+
+}
