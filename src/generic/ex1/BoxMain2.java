fa9ace21d49f5f1d2bcc94527908c370d57a7370
hj <parkhj062@gmail.com>
2025-12-29 17:54:16 +0900

    [6. 다형성을 통한 중복 해결 시도]
    [Object]
    - code reusability
    - type unsafety(down casting)

---
+++ src/generic/ex1/BoxMain2.java
@@ -0,0 +1,20 @@
+package generic.ex1;
+
+public class BoxMain2 {
+    public static void main(String[] args) {
+        ObjectBox integerBox = new ObjectBox();
+        integerBox.set(10);
+        Integer integer = (Integer) integerBox.get();
+        System.out.println(integer);
+
+        ObjectBox stringBox = new ObjectBox();
+        stringBox.set("Hello");
+        String string = (String) stringBox.get();
+        System.out.println(string);
+
+        // runtime mistake
+        integerBox.set("11");
+        Integer result = (Integer) integerBox.get();
+        System.out.println(result);
+    }
+}
