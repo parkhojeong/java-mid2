bf2364917605364a20c6cf7d98f0e9d4b7d31244
hj <parkhj062@gmail.com>
2025-12-29 19:29:36 +0900

    [12. 타입 매개변수 제한2 - 다형성 시도]
    - parent type class
    - type unsafety

---
+++ src/generic/test/ex3/AnimalHospitalMainV1.java
@@ -0,0 +1,29 @@
+package generic.test.ex3;
+
+import generic.animal.Animal;
+import generic.animal.Cat;
+import generic.animal.Dog;
+
+public class AnimalHospitalMainV1 {
+    public static void main(String[] args) {
+        AnimalHospitalV1 dogHospital = new AnimalHospitalV1();
+        AnimalHospitalV1 catHospital = new AnimalHospitalV1();
+
+        Dog dog = new Dog("dog1", 100);
+        Cat cat = new Cat("cat1", 200);
+
+        dogHospital.set(dog);
+        dogHospital.checkup();
+
+        // runtime error
+        dogHospital.set(cat);
+
+        catHospital.set(cat);
+        catHospital.checkup();
+
+        // type unsafety: down-casting error possible
+        Dog largerDog = (Dog) dogHospital.bigger(new Dog("dog2", 200));
+        System.out.println("largerDog = " + largerDog);
+
+    }
+}
---
+++ src/generic/test/ex3/AnimalHospitalV1.java
@@ -0,0 +1,26 @@
+package generic.test.ex3;
+
+import generic.animal.Animal;
+
+public class AnimalHospitalV1 {
+    private Animal animal;
+
+    public void set(Animal animal) {
+        this.animal = animal;
+    }
+
+    public Animal get() {
+        return animal;
+    }
+
+    public void checkup() {
+        System.out.println(animal.getName());
+        System.out.println(animal.getSize());
+        animal.sound();
+    }
+
+    public Animal bigger(Animal target) {
+        return animal.getSize() > target.getSize() ? animal : target;
+    }
+
+}
