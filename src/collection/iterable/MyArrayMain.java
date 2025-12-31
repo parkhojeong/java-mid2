fe474818a5041d75feadef581e84b1993759fa85
hj <parkhj062@gmail.com>
2025-12-31 20:08:22 +0900

    [84. 순회2 - 향상된 for문]
    - iterable for enhanced for loop

--- src/collection/iterable/MyArrayMain.java
+++ src/collection/iterable/MyArrayMain.java
@@ -6,8 +6,14 @@ public class MyArrayMain {
     public static void main(String[] args) {
         MyArray myArray = new MyArray(new int[]{1, 2, 3, 4, 5});
         Iterator<Integer> iterator = myArray.iterator();
+        System.out.println("== iterator ==");
         while(iterator.hasNext()) {
             System.out.println(iterator.next());
+        }
+
+        System.out.println("== enhanced for loop ==");
+        for (int value : myArray) {
+            System.out.println("value = " + value);
         }
     }
 }
